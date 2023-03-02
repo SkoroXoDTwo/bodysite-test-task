9e953ac41bdee3ac8df5490b9b23628d17a3bd4e
Alexey Belkov <lexabelco2013@yandex.ru>
2023-03-02 23:32:27 +0500

    feat: сверстать hits

--- components/CatalogCard.js
+++ components/CatalogCard.js
@@ -3,11 +3,11 @@ export class CatalogCard {
     this._title = data.title;
     this._src = data.src;
     this._count = data.count;
+    this._link = data.link ? data.link : '#';
     this._templateCard = template;
   }
 
   _getTemplate() {
-    console.log(this._templateCard);
     const cardElement = document
       .querySelector(this._templateCard)
       .content.querySelector(".catalog__list-item")
@@ -18,13 +18,16 @@ export class CatalogCard {
 
   generateCard() {
     this._element = this._getTemplate();
-    this._titleElement = this._element.querySelector('.catalog__card-title');
-    this._imgElement = this._element.querySelector('.catalog__card-img');
-    this._countElement = this._element.querySelector('.catalog__card-count');
+    this._titleElement = this._element.querySelector(".catalog__card-title");
+    this._imgElement = this._element.querySelector(".catalog__card-img");
+    this._countElement = this._element.querySelector(".catalog__card-count");
+    this._linkElement = this._element.querySelector(".catalog__list-item-link");
 
     this._titleElement.textContent = this._title;
     this._imgElement.src = this._src;
+    this._imgElement.alt = this._title;
     this._countElement.textContent = this._count;
+    this._linkElement.href = this._link;
 
     return this._element;
   }
